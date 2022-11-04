5983fcfa612a5eb134b7be417b6f904d3f1d1b38
FelipeBigarelli <felipebigarelli@hotmail.com>
2022-11-04 09:30:19 -0300

    Add AppErrors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
+import 'express-async-errors';
 
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -16,6 +18,22 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
